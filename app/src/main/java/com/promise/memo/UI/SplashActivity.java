d88cdb0d8562a67d4547c0dd7edf2fa04d730f6b
moujunqiang <705174710@qq.com>
2020-11-30 18:33:40 +0800

    Initial commit

---
+++ app/src/main/java/com/promise/memo/UI/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.promise.memo.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                boolean aBoolean = getSharedPreferences("login", MODE_PRIVATE).getBoolean("login", false);
+                if (!aBoolean) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        }, 1500);
+
+
+    }
+}
